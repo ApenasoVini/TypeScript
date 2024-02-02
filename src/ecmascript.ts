704610b369b38fa6a283645fc65f2d2b66c66062
vi058587 <vinicius.franca@intelbras.com>
2024-02-02 14:35:43 -0300

    Typescript

---
+++ src/ecmascript.ts
@@ -0,0 +1,46 @@
+// let & const
+// let teste = '?'
+// console.log(teste);
+
+// let verdade = true
+// if (verdade) {
+//     let y = 'Verdade'
+//     console.log(y)
+// }
+
+// const cpf:string = 'xxx.xxx.xxx-xx'
+// cpf = 'yyy.yyy.yyy-yy'
+// console.log(cpf)
+
+// function revelar () {
+//     const segredo = 'x'
+//     console.log(segredo)
+// }
+// revelar()
+// console.log(segredo)
+
+//Arrow Function
+// function somar(n1: number, n2: number): number {
+//     return n1 + n2
+// }
+// console.log(somar(1,2))
+
+// const subtrair = (n1:number, n2:number) => n1 - n2
+// console.log(subtrair(5,1))
+
+// const x = () => console.log("Oi")
+// x()
+
+// const y = (numero: number) => console.log(`Número ${numero}`)
+// console.log(y(4))
+
+//Parâmetro padrão
+// function contagem(inicio: number = 0): void {
+//     console.log(inicio)
+//     while(inicio < 10){
+//         inicio++
+//         console.log(inicio)
+//     }
+//     console.log("fim")
+// }
+// contagem()
